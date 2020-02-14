41d1c3dde39434eb649866b296803ec6ff101fde
Keith Lueneburg <keith.lueneburg@gmail.com>
2020-02-13 21:23:41 -0800

    Add function to get SMBIOS data

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,47 @@
-use winapi::shared::minwindef::UINT;
-use winapi::shared::ntdef::NULL;
-use winapi::shared::minwindef::DWORD;
-use winapi::um::sysinfoapi::GetSystemFirmwareTable;
+
 
 fn main() {
     println!("Hello, world!");
 
-    let rsmb_sig : DWORD = 0x52534d42; // 'RSMB'
-    let rsmb_tbl_id : DWORD = 0x00000000;
-    
-    let sz : UINT;
-    let read_sz : UINT;
-    unsafe
-    {
-        sz = GetSystemFirmwareTable(rsmb_sig, rsmb_tbl_id, NULL, 0);
-    };
-    println!("Need size {}", sz);
+    let data = smbios_windows::read_smbios();
 
-    let mut vec = Vec::with_capacity(sz as usize);
-    unsafe
-    {
-        read_sz = GetSystemFirmwareTable(rsmb_sig, rsmb_tbl_id, vec.as_mut_ptr(), sz);
+    println!("Data size: {}", data.len());
+
+    for b in data.iter() {
+        println!("{}", b);
     }
-    println!("Read size {}", read_sz);
-    assert!(sz == read_sz);
-    
+}
+
+mod smbios_windows {
+    use winapi::shared::minwindef::DWORD;
+    use winapi::shared::minwindef::UINT;
+    use winapi::shared::ntdef::NULL;
+    use winapi::ctypes::c_void;
+    use winapi::um::sysinfoapi::GetSystemFirmwareTable;
+
+pub fn read_smbios() -> Vec<u8> {
+        let rsmb_sig : DWORD = 0x52534d42; // 'RSMB'
+        let rsmb_tbl_id : DWORD = 0x00000000;
+        
+        let sz : UINT;
+        let read_sz : UINT;
+        unsafe
+        {
+            sz = GetSystemFirmwareTable(rsmb_sig, rsmb_tbl_id, NULL, 0);
+        };
+        println!("Need size {}", sz);
+
+        let mut vec : Vec<u8> = Vec::with_capacity(sz as usize);
+        unsafe { vec.set_len(sz as usize); }
+
+        unsafe
+        {
+            read_sz = GetSystemFirmwareTable(rsmb_sig, rsmb_tbl_id, vec.as_mut_ptr() as *mut c_void, sz);
+        }
+        println!("Read size {}", read_sz);
+        assert!(sz == read_sz);
+        
+        // Return buffer
+        vec
+    }
 }
