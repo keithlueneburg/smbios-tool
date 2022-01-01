b980c61f2418be39427e2d0baea8f0498177f4ca
Keith Lueneburg <keith.lueneburg@gmail.com>
2022-01-01 09:39:19 -0800

    Clean up get strings function

--- src/tables.rs
+++ src/tables.rs
@@ -6,21 +6,21 @@ pub fn get_table_strings(data: &[u8], start: usize) -> Vec<String> {
     let mut buf = String::from("");
     let mut strings : Vec<String> = Vec::new();
 
-    let mut ch = data[pos] as char;
-    while ch != 0x00 as char {
-        buf.push(ch);
+    let mut ch = data[pos];
+    while ch != 0x00 {
+        buf.push(ch as char);
 
         pos += 1;
-        ch = data[pos] as char;
+        ch = data[pos];
 
-        if ch == 0x00 as char {
+        if ch == 0x00 {
             // 0 indicates end of string
             // 2nd 0 terminating table string section will get caught by loop
             strings.push(buf.clone());
             buf.clear();
 
             pos += 1; 
-            ch = data[pos] as char;
+            ch = data[pos];
         }
     }
 	return strings;
